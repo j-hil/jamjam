91f96006862ae44359ea9461b182ae40f8f6472d
j-hil <92876062+j-hil@users.noreply.github.com>
2025-04-29 03:43:03 +0100

    New utils `typing_only`, `HintWrap`, `Check`

--- jamjam/_tests/test_typing.py
+++ jamjam/_tests/test_typing.py
@@ -1,13 +1,22 @@
 import inspect
 from collections.abc import Callable
-from typing import Any, assert_type, overload
+from typing import (
+    Any,
+    Literal,
+    TypeAlias,
+    Union,
+    assert_type,
+    overload,
+)
 
 from pytest import raises
 
 from jamjam.typing import (
+    Check,
     Seq,
     copy_params,
     copy_type,
+    typing_only,
     use_overloads,
 )
 
@@ -59,3 +68,37 @@ def test_use_overloads() -> None:
 
     with raises(TypeError):
         f(z="a", y="b")  # type: ignore[call-overload]
+
+
+def test_has_instance() -> None:
+    Option: TypeAlias = Literal[1, 2, 3]
+    x: Literal[Option, 4]
+    for x in (1, 2, 3, 4):
+        if Check[Option].has_instance(x):
+            assert_type(x, Option)
+            assert x in {1, 2, 3}
+        else:
+            assert_type(x, Literal[4])
+            assert x == 4
+
+    U: TypeAlias = int | str
+    assert Check[U].has_instance(1)
+    assert Check[U].has_instance("")
+    assert not Check[U].has_instance([])
+
+    V: TypeAlias = Union[str, list, tuple]  # noqa: UP007
+    assert Check[V].has_instance([])
+
+
+def test_typing_only() -> None:
+    class B:
+        def f(self) -> int:
+            _ = self
+            return 1
+
+    class A(B):
+        @typing_only
+        def f(self) -> int:
+            raise NotImplementedError
+
+    assert A().f() == 1
