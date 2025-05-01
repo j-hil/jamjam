1430cd66a1b4d50103ca1c17f561d28b83179fd8
j-hil <92876062+j-hil@users.noreply.github.com>
2025-05-01 19:01:36 +0100

    New `win` top level + `win.api`

--- jamjam/_tests/test_win.py
+++ jamjam/_tests/test_win.py
@@ -1,10 +1,10 @@
 from jamjam._tests.conftest import manual_only
-from jamjam.win import (
+from jamjam.win import Vk, write
+from jamjam.win.api import (
     Input,
     InputType,
     KeybdInput,
     Mb,
-    Vk,
     user32,
 )
 
@@ -26,3 +26,15 @@ def test_send_input() -> None:
         type=InputType.KEYBOARD, ki=KeybdInput(wVk=Vk.A)
     )
     user32.SendInput(1, (Input * 1)(press_a), Input.size())
+
+
+@manual_only
+def test_vk() -> None:
+    "Should type out 'a' where cursor is."
+    Vk.A.down()
+
+
+@manual_only
+def test_write() -> None:
+    "Writes text where cursor is."
+    write("Hello!")
